537b49a650622ae523772b05dea70af274717d52
Alejandra Jurado <ajurado2204@gmail.com>
2015-11-22 20:19:52 -0500

    finished todolist

--- js/events.js
+++ js/events.js
@@ -2,16 +2,26 @@ $(document).ready(initializer());
 
 function initializer(){
 	
-	$('.check').click(function(){
-    $(this).closest('tr').children(":first-child").css(
-        'text-decoration',
-        this.checked ? 'line-through' : 'none'
-    ).next().text("");
-	});
-
-	$('.btn.btn-primary.btn-sm').click(function(){
+	$(document).on("click", '.mydelete',function(){
 		$(this).closest('tr').hide();
 	});
 
+	$('#addbutton').click(function() {
+		if($('textarea.form-control').val() === ""){
+			alert("Please enter your task");
+		}else{
+			var thingsToDo = $('textarea.form-control').val();
+			console.log(thingsToDo);
+			$('table').find('tbody').after(
+				'<tr><td>'+thingsToDo+'</td><td><input type="checkbox" class="check"> Checkbox</td><td><a href="#" class="btn btn-primary btn-sm mydelete">Delete</a></td></tr>');
+		}
+	});
+
+		$(document).on("click", '.check',function(){
+			$(this).closest('tr').children(":first-child").css(
+        'text-decoration',
+        this.checked ? 'line-through' : 'none'
+    	).next().text("");
+		});
 
 }
